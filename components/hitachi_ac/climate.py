258de5866dfcdc5754730b374df5360a412338cf
Sai Khurana <saikhurana1@gmail.com>
2025-04-13 20:27:40 +0530

    add cg stuff for our custom climate implementation

--- components/hitachi_ac/climate.py
+++ components/hitachi_ac/climate.py
@@ -1,3 +1,4 @@
+import esphome.codegen as cg
 import esphome.config_validation as cv
 from esphome.components import climate
 from esphome.const import (
@@ -10,23 +11,17 @@ from esphome.const import (
 )
 import voluptuous as vol
 
-# Define the configuration schema for the standard climate component.
-# We extend the base CLIMATE_SCHEMA and add any component-specific parameters.
+AUTO_LOAD = ["climate"]
+hitachi_ac_ns = cg.esphome_ns.namespace("hitachi_ac")
+HitachiAC = hitachi_ac_ns.class_("HitachiAC",climate.Climate)
+
 CONFIG_SCHEMA = climate.CLIMATE_SCHEMA.extend({
-    cv.GenerateID(): cv.declare_id(),
-    vol.Optional(CONF_NAME, default="IR AC"): cv.string,
-    # Example of a custom parameter. For instance, specifying an initial or default target temperature.
+    cv.GenerateID(): cv.declare_id(HitachiAC),
+    vol.Optional(CONF_NAME, default="Hitachi AC"): cv.string,
     vol.Optional("default_temperature", default=24.0): cv.temperature,
 }).extend(cv.COMPONENT_SCHEMA)
 
-def to_code(config):
-    # Create the C++ instance of our IR AC climate component.
-    # Here we assume you have a constructor function that creates a new HitachiACClimate instance.
-    var = yield climate.new_climate_device(config[CONF_ID], config[CONF_NAME])
-    # Set the default temperature if provided.
-    if "default_temperature" in config:
-        temp = config["default_temperature"]
-        # This setter function must be implemented in your C++ class (HitachiACClimate).
-        var.set_default_temperature(temp)
-    # Register the component so it becomes part of the ESPHome configuration.
-    yield climate.register_climate_device(var, config)
+
+async def to_code(var, config):
+    await cg.register_component(var,config)
+    await climate.register_climate(var, config)
