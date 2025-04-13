de59d291c3cc6e07f808660457d3b87bca902b58
Sai Khurana <saikhurana1@gmail.com>
2025-04-13 22:34:57 +0530

    possible minor fix

--- components/hitachi_ac/climate.py
+++ components/hitachi_ac/climate.py
@@ -1,15 +1,7 @@
 import esphome.codegen as cg
 import esphome.config_validation as cv
 from esphome.components import climate
-from esphome.const import (
-    CONF_ID,
-    CONF_NAME,
-    CLIMATE_MODE_AUTO,
-    CLIMATE_MODE_COOL,
-    CLIMATE_MODE_HEAT,
-    CLIMATE_MODE_OFF,
-)
-import voluptuous as vol
+from esphome.const import (CONF_ID)
 
 AUTO_LOAD = ["climate"]
 
@@ -19,8 +11,8 @@ HitachiAC = hitachi_ac_ns.class_("HitachiACClimate",climate.Climate)
 CONFIG_SCHEMA = climate.CLIMATE_SCHEMA.extend(
     {
     cv.GenerateID(): cv.declare_id(HitachiAC),
-}
-).extend(cv.COMPONENT_SCHEMA)
+    }
+)
 
 
 async def to_code(config):
