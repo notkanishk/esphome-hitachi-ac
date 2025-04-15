e8c0c854a1f7761ef99a75b53a64c762ee866596
Sai Khurana <saikhurana1@gmail.com>
2025-04-15 21:37:05 +0530

    refactor  lg component

--- components/climate_lgg/climate.py
+++ components/climate_lgg/climate.py
@@ -1,19 +1,20 @@
 import esphome.codegen as cg
-from esphome.components import climate
 import esphome.config_validation as cv
+from esphome.components import climate
 from esphome.const import (CONF_ID)
 
 AUTO_LOAD = ["climate"]
 
-climate_lg2_ns = cg.esphome_ns.namespace("climate_lg2")
-ClimateLG2 = climate_lg2_ns.class_("ClimateLG2",climate.Climate)
+climate_lgg_ns = cg.esphome_ns.namespace("climate_lgg")
+ClimateLGG = climate_lgg_ns.class_("ClimateLGG",climate.Climate)
 
 CONFIG_SCHEMA = climate.CLIMATE_SCHEMA.extend(
     {
-        cv.GenerateID: cv.declare_id(ClimateLG2)
+    cv.GenerateID(): cv.declare_id(ClimateLGG),
     }
 )
 
+
 async def to_code(config):
     var = cg.new_Pvariable(config[CONF_ID])
-    await climate.register_climate(var, config)
+    await climate.register_climate(var,config)
