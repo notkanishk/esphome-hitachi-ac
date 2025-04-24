f643649033b5bc934a3f7d7467872b0244b858ec
Sai Khurana <saikhurana1@gmail.com>
2025-04-25 04:05:44 +0530

    refactor & bugfix

---
+++ components/lg_climate/climate.py
@@ -0,0 +1,36 @@
+import esphome.codegen as cg
+import esphome.config_validation as cv
+from esphome.components import climate
+from esphome.const import (
+    CONF_ID,
+)
+from esphome import pins
+
+DEPENDENCIES = ["esp32"]
+
+lg_climate_ns = cg.esphome_ns.namespace("lg_climate")
+LGClimate = lg_climate_ns.class_("LGClimate", climate.Climate, cg.Component)
+
+CONF_TRANSMITTER_PIN = "transmitter_pin"
+CONF_RECEIVER_PIN = "receiver_pin"
+
+CONFIG_SCHEMA = climate.CLIMATE_SCHEMA.extend({
+    cv.GenerateID(): cv.declare_id(LGClimate),
+    cv.Required(CONF_TRANSMITTER_PIN): pins.gpio_output_pin_schema,
+    cv.Optional(CONF_RECEIVER_PIN): pins.gpio_input_pin_schema,
+}).extend(cv.COMPONENT_SCHEMA)
+
+async def to_code(config):
+    var = cg.new_Pvariable(config[CONF_ID])
+    await cg.register_component(var, config)
+    await climate.register_climate(var, config)
+
+    transmitter_pin = await cg.gpio_pin_expression(config[CONF_TRANSMITTER_PIN])
+    cg.add(var.set_transmitter_pin(transmitter_pin))
+
+    if CONF_RECEIVER_PIN in config:
+        receiver_pin = await cg.gpio_pin_expression(config[CONF_RECEIVER_PIN])
+        cg.add(var.set_receiver_pin(receiver_pin))
+
+    # Add IRremoteESP8266 library dependency
+    cg.add_library("crankyoldgit/IRremoteESP8266", "2.8.6")
---
+++ components/lg_climate/lg_climate.h
@@ -0,0 +1,40 @@
+#pragma once
+
+#include "esphome/core/component.h"
+#include "esphome/components/climate/climate.h"
+#include "esphome/core/gpio.h"
+
+// Include IRremoteESP8266 library headers
+#include <IRremoteESP8266.h>
+#include <IRsend.h>
+#include <IRrecv.h>
+#include <IRutils.h>
+#include <ir_LG.h>
+
+namespace esphome {
+namespace lg_climate {
+
+class LGClimate : public climate::Climate, public Component {
+ public:
+  void setup() override;
+  void loop() override;
+  void control(const climate::ClimateCall &call) override;
+  climate::ClimateTraits traits() override;
+
+  void set_transmitter_pin(InternalGPIOPin *pin) { transmitter_pin_ = pin; }
+  void set_receiver_pin(InternalGPIOPin *pin) { receiver_pin_ = pin; }
+
+ protected:
+  IRLgAc *ac_{nullptr};
+  IRsend *ir_send_{nullptr};
+  IRrecv *ir_recv_{nullptr};
+  decode_results results_{};
+  InternalGPIOPin *transmitter_pin_{nullptr};
+  InternalGPIOPin *receiver_pin_{nullptr};
+
+  // Handle decoded IR signals
+  void decode_and_update();
+};
+
+}  // namespace lg_climate
+}  // namespace esphome
