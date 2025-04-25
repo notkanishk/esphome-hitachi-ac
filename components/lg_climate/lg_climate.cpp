30779fa818651aac2a8dc7cc5d9dfe911183ca47
Sai Khurana <44598300+saikhurana98@users.noreply.github.com>
2025-04-26 05:21:27 +0530

    add climate_mode_off

--- components/lg_climate/lg_climate.cpp
+++ components/lg_climate/lg_climate.cpp
@@ -121,6 +121,7 @@ namespace esphome
       auto traits = climate::ClimateTraits();
       traits.set_supports_current_temperature(false);
       traits.set_supported_modes({
+          climate::CLIMATE_MODE_OFF,
           climate::CLIMATE_MODE_AUTO,
           climate::CLIMATE_MODE_COOL,
           climate::CLIMATE_MODE_FAN_ONLY,
